883ba4656e70216af2b132dd711e3380d1bb0864
Alin Olteanu <olteanualin1509@gmail.com>
2022-11-26 14:01:22 +0200

    Add to card pt3

--- controllers/cart.js
+++ controllers/cart.js
@@ -2,21 +2,36 @@ import Cart from "../models/cart.js";
 import Category from "../models/categories.js";
 import Product from "../models/products.js";
 
+
+
+//              GET
+
 export async function getCartItems(req, res){
     const cartItems = await Cart.find();
     const categories = (await Category.distinct('name')).reverse();
     const products = [];
+    let totalPrice = 0;
 
     for(const item of cartItems){
-        console.log(item.productId);
         let addProduct = await Product.findById(item.productId).lean();
         addProduct.quantity = item.quantity;
-        console.log(typeof addProduct);
+        totalPrice += addProduct.price * addProduct.quantity;
+        addProduct.cartId = item._id;
         products.push(addProduct);
     }
 
-    res.render('cart.ejs', {categories, products});
+    res.render('cart.ejs', {categories, products, totalPrice});
 }
+
+export async function getOrderConfirmation(req, res){
+    const categories = (await Category.distinct('name')).reverse();
+
+    res.render('order-confirmation.ejs', {categories});
+}
+
+
+
+//              POST
 
 export async function postCartItem(req, res){
     const item = req.body;
@@ -27,7 +42,6 @@ export async function postCartItem(req, res){
         }else{
             item.quantity = 1;
             const cartItem = new Cart(item);
-            console.log(cartItem);
             await cartItem.save();
         }
 
@@ -36,6 +50,25 @@ export async function postCartItem(req, res){
         res.status(400).json(error.message);
     }
 }
+
+export async function postOrder(req, res){
+    console.log("Comanda noua:")
+    console.log(req.body);
+
+    try {
+        await Cart.deleteMany({});
+
+        res.status(200).redirect("/cart/order");
+    } catch (error) {
+        res.status(400).json(error.message);
+    }
+
+}
+
+
+
+
+//              DELETE
 
 export async function deleteCartItem(req, res){
     const {id} = req.body;
--- routes/cart.js
+++ routes/cart.js
@@ -1,11 +1,15 @@
 import express from 'express';
-import {deleteCartItem, getCartItems, postCartItem} from "../controllers/cart.js";
+import {deleteCartItem, getCartItems, postCartItem, postOrder, getOrderConfirmation} from "../controllers/cart.js";
 const router = express.Router();
 
 router.route('/')
     .get(getCartItems)
     .post(postCartItem)
     .delete(deleteCartItem);
+
+router.route('/order')
+    .get(getOrderConfirmation)
+    .post(postOrder);
 
 
 export default router;
